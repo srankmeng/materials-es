184f16f5ce3bc808399731e175a7a0345ccdd225
srankmeng <jaturong.duang@gmail.com>
2023-03-09 17:26:17 +0700

    Create layout

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -2,17 +2,115 @@
 <template>
   <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
-    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
-      <a class="flex justify-center pt-8 sm:pt-0" href="https://nuxtjs.org" target="_blank">
-        <svg width="218" height="45" viewBox="0 0 159 30" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M55.5017 6.81866H60.1727L70.0719 22.9912V6.81866H74.3837V29.7345H69.7446L59.8135 13.5955V29.7345H55.5017V6.81866Z" fill="#003543" /> <path d="M93.657 29.7344H89.6389V27.1747C88.7241 28.9761 86.8628 29.9904 84.5113 29.9904C80.7869 29.9904 78.3684 27.3059 78.3684 23.4423V13.2339H82.3865V22.5976C82.3865 24.8566 83.7594 26.4276 85.8171 26.4276C88.0712 26.4276 89.6389 24.6598 89.6389 22.2377V13.2339H93.657V29.7344Z" fill="#003543" /> <path d="M107.64 29.7344L103.784 24.2342L99.9291 29.7344H95.6492L101.596 21.1242L96.1074 13.2339H100.485L103.784 17.9821L107.051 13.2339H111.461L105.94 21.1242L111.886 29.7344H107.64Z" fill="#003543" /> <path d="M120.053 8.25848V13.2339H124.627V16.6063H120.053V24.7974C120.053 25.0725 120.162 25.3363 120.356 25.531C120.55 25.7257 120.813 25.8353 121.087 25.8357H124.627V29.728H121.98C118.386 29.728 116.035 27.6323 116.035 23.9687V16.6095H112.801V13.2339H114.83C115.776 13.2339 116.327 12.6692 116.327 11.7349V8.25848H120.053Z" fill="#003543" /> <path d="M134.756 24.5446V6.81866H139.066V23.1864C139.066 27.6067 136.943 29.7345 133.349 29.7345H128.332V25.8421H133.461C133.804 25.8421 134.134 25.7054 134.377 25.4621C134.619 25.2188 134.756 24.8888 134.756 24.5446Z" fill="#003543" /> <path d="M141.649 22.0409H145.799C146.029 24.6006 147.728 26.2308 150.472 26.2308C152.923 26.2308 154.623 25.2501 154.623 23.2199C154.623 18.3085 142.331 21.7129 142.331 12.9395C142.334 9.17515 145.568 6.55945 150.215 6.55945C155.05 6.55945 158.317 9.34153 158.516 13.6306H154.388C154.193 11.6341 152.632 10.2918 150.207 10.2918C147.953 10.2918 146.548 11.3397 146.548 12.9427C146.548 18.0173 159 14.2226 159 23.1576C159 27.4131 155.504 30 150.474 30C145.279 30 141.882 26.8563 141.654 22.0441" fill="#003543" /> <path d="M24.7203 29.704H41.1008C41.6211 29.7041 42.1322 29.5669 42.5828 29.3061C43.0334 29.0454 43.4075 28.6704 43.6675 28.2188C43.9275 27.7672 44.0643 27.2549 44.0641 26.7335C44.0639 26.2121 43.9266 25.6999 43.6662 25.2485L32.6655 6.15312C32.4055 5.70162 32.0315 5.32667 31.581 5.06598C31.1305 4.8053 30.6195 4.66805 30.0994 4.66805C29.5792 4.66805 29.0682 4.8053 28.6177 5.06598C28.1672 5.32667 27.7932 5.70162 27.5332 6.15312L24.7203 11.039L19.2208 1.48485C18.9606 1.03338 18.5864 0.658493 18.1358 0.397853C17.6852 0.137213 17.1741 0 16.6538 0C16.1336 0 15.6225 0.137213 15.1719 0.397853C14.7213 0.658493 14.3471 1.03338 14.0868 1.48485L0.397874 25.2485C0.137452 25.6999 0.000226653 26.2121 2.8053e-07 26.7335C-0.000226092 27.2549 0.136554 27.7672 0.396584 28.2188C0.656614 28.6704 1.03072 29.0454 1.48129 29.3061C1.93185 29.5669 2.44298 29.7041 2.96326 29.704H13.2456C17.3195 29.704 20.3239 27.9106 22.3912 24.4118L27.4102 15.7008L30.0986 11.039L38.1667 25.0422H27.4102L24.7203 29.704ZM13.0779 25.0374L5.9022 25.0358L16.6586 6.36589L22.0257 15.7008L18.4322 21.9401C17.0593 24.2103 15.4996 25.0374 13.0779 25.0374Z" fill="#00DC82" /></svg>
-      </a>
-      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="getUsers">
-          Get Users
-        </button>
-        <p class="mt-3 text-gray-600" v-for="u in users">{{ u.firstName }} {{ u.lastName }}</p>
+    <div class="mx-auto p-8 bg-gray-200">
+     
+      <div class="m-16">
+        <img class="w-24 h-24 mx-auto ..." src="../static/logo.png">
+      </div>
+      
+      <div class="grid grid-cols-3 gap-4 mb-4">
+        <div class="col-span-2 ...">
+          <input class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Please type Spec no., Spec issue no., Spec name">
+        </div>
+      </div>
+
+      <label class="block">
+        <span class="block text-sm font-medium text-slate-700">Filter</span>
+      </label>
+      <div class="grid grid-cols-4 gap-4">
+        <div class="...">
+          <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+            <option>All Status</option>
+            <option>Approve</option>
+            <option>Draft</option>
+          </select>
+        </div>
+        <div class="...">
+          <select class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+            <option>All Effective Date</option>
+            <option>11 Nov 2011 - 11 Nov 2022</option>
+          </select>
+        </div>
+        <div class="...">
+          <input class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Corss reference no.">
+        </div>
+        <div class="...">
+          <input class="appearance-none block w-full border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" placeholder="Originator name">
+        </div>
+      </div>
+
+      <div class="grid grid-cols-4 gap-4">
+        <div class="...">
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="search">
+            Search
+          </button>
+        </div>
+      </div>
+
+      <div class="relative overflow-x-auto mt-16">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-3">
+                Spec no.
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Spec issue no.
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Spec name
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Status
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Effective date
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Cross reference
+              </th>
+              <th scope="col" class="px-6 py-3">
+                Originator
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-for="m in materials">
+              <td class="px-6 py-4">
+                {{ m.spec_no }}
+              </td>
+              <td class="px-6 py-4">
+                {{  m.spec_issue_no }}
+              </td>
+              <td class="px-6 py-4">
+                {{  m.spec_name }}
+              </td>
+              <td class="px-6 py-4">
+                {{  m.status }}
+              </td>
+              <td class="px-6 py-4">
+                {{  m.effective_date }}
+              </td>
+              <td class="px-6 py-4">
+                {{  m.cross_reference }}
+              </td>
+              <td class="px-6 py-4">
+                {{  m.originator }}
+              </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700" v-if="!materials.length">
+              <td class="px-6 py-4" colspan="7">
+                There is no data
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
+
+
+    
+
   </div>
 </template>
 
@@ -22,14 +120,14 @@ export default {
 
   data() {
     return {
-      users: [],
+      materials: [],
     }
   },
 
   methods: {
-    async getUsers() {
+    async search() {
       const res = await this.$axios.get(`${this.$config.apiUrl}/users`)
-      this.users = res?.data || []
+      this.materials = res?.data || []
     },
   }
 }
